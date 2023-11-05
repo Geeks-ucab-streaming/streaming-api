accf37865c79e2b3d18a2ac78af34cb259914259
gamboaalejandro <agamboacj@gmail.com>
2023-11-05 15:21:00 -0400

    swagger agg

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,12 @@
 /* eslint-disable prettier/prettier */
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  
+  @ApiTags('Hello')
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { swagger } from './docs/swagger.docs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  swagger(app);
   await app.listen(3000);
 }
 bootstrap();
