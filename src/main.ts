b20233d02776527e56cb67ca336b06cf7a39de10
gamboaalejandro <agamboacj@gmail.com>
2023-11-08 21:47:25 -0400

    adding secrets in git

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,6 @@ dotenv.config({path: `./deploy/.env.${process.env.NODE_ENV}`});
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { swagger } from './docs/swagger.docs';
-
-
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
