fe442e4b3eabeaa1647b75ea99774cbbcdb3cc3e
gamboaalejandro <agamboacj@gmail.com>
2023-11-08 21:58:58 -0400

    adding secrets in git

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,17 +1,13 @@
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
-// IMPORTAR EL CONFIG SERVICES DEN NEST PARA USAR LOS ENVS
-import { ConfigService } from '@nestjs/config';
 
-// INSTANCIAR EL CONFIG SERVICE
-const configService = new ConfigService();
-console.log(configService.get('HOST'));
+
 const config: PostgresConnectionOptions = {
     type: 'postgres',
-    host: configService.get('HOST'),
-    port: parseInt(configService.get('PORT')),
-    username: configService.get('BD_USERNAME'),
-    password: configService.get('PASSWORD'),
-    database: configService.get('DATABASE'),
+    host: process.env.HOST,
+    port: Number(process.env.PORT),
+    username: process.env.BD_USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
     entities: [],
     synchronize: false,
     ssl: {
