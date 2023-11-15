55e317ca1f275943070e74e4c76f5dd191439560
diegoargottez@gmail.com <diegoargottez@gmail.com>
2023-11-15 16:58:55 -0400

    [imp]:Find Playlist by playlist id (images missing)

--- src/playlist/infrastructure/controllers/playlist.controller.ts
+++ src/playlist/infrastructure/controllers/playlist.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Inject, Param } from "@nestjs/common";
 import { FindAlbumByArtistIDService } from "src/playlist/application/dtos/services/FindAlbumByArtistID";
+import { FindAlbumByPlaylistIDService } from "src/playlist/application/dtos/services/FindAlbumByPlaylistID.service";
 import { Playlist } from "src/playlist/domain/playlist";
 
 @Controller('playlists')
@@ -7,6 +8,8 @@ export class PlaylistController {
   constructor(
     @Inject('FindOnePlaylistService')
     private readonly findOnePlaylistService: FindAlbumByArtistIDService,
+    @Inject('FindPlaylistByIdService')
+    private readonly findPlaylistByIdService: FindAlbumByPlaylistIDService,
 
   ) {}
 
@@ -18,5 +21,10 @@ export class PlaylistController {
   @Get('/FindByArtistID/:id')
   find(@Param('id') id: string): Promise<Playlist> {
     return this.findOnePlaylistService.execute(id);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Promise<Playlist> {
+    return this.findPlaylistByIdService.execute(id);
   }
 }
