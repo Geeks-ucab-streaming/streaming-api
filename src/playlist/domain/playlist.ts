e6139da443ab885a882837239773e7341b842cb1
diegoargottez@gmail.com <diegoargottez@gmail.com>
2023-11-15 22:39:46 -0400

    [imp]: attr to playlist class

--- src/playlist/domain/playlist.ts
+++ src/playlist/domain/playlist.ts
@@ -1,3 +1,6 @@
+import { Artist } from "src/artists/domain/artist";
+import { Song } from "src/songs/domain/song";
+
 export class Playlist {
   id: string;
   name: string;
@@ -5,7 +8,8 @@ export class Playlist {
   image_reference: string;
   playlist_Image: Buffer | null;
   reproductions: number;
-
+  playlistCreator?: Artist[] | null;
+  playlistSong?: Song[] | null;
   constructor(
     id: string,
     name: string,
