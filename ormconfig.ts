d8d2da8b012e57959051be6f9c48358144b4233f
gamboaalejandro <agamboacj@gmail.com>
2023-11-18 03:02:42 -0400

    update ssl config to accept local conections

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,14 +1,18 @@
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
+const isProduction = process.env.NODE_ENV === 'local';
+
+const sslConfig = isProduction
+    ? {ssl:false}
+    : { ssl: { rejectUnauthorized: false } } ;
+
 const config: PostgresConnectionOptions = {
   type: 'postgres',
   url: process.env.HOST,
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
   synchronize: true,
-  ssl:{
-    rejectUnauthorized: false
-  }
+  ...sslConfig
 };
 
 export default config;
