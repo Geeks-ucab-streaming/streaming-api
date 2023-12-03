fd198315049fbed7a762e7a15fa04f9e8509f9e7
Andrés <95057542+andreselc@users.noreply.github.com>
2023-12-03 18:21:27 -0400

    userName ValueObject implemented

--- src/users/domain/userAggregate/value-objects/userName.ts
+++ src/users/domain/userAggregate/value-objects/userName.ts
@@ -1,4 +1,16 @@
 export class userName {
-    
-    
+  private name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  public getValue(): string {
+    return this.name;
+  }
+
+  public equals(username: userName): boolean {
+    return this.name === username.getValue();
+  }
+
 }
