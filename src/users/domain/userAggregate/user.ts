1b73b1042d9cade3afb9f19271fca82eee6a4047
Andrés <95057542+andreselc@users.noreply.github.com>
2023-12-03 19:29:24 -0400

    user.ts with their value objects and its entity

--- src/users/domain/userAggregate/user.ts
+++ src/users/domain/userAggregate/user.ts
@@ -1,15 +1,17 @@
-import { Phone } from "src/phones/domain/phone";
 import { PhonesNumber } from "./value-objects/phoneNumber";
 import { PhoneEntity } from "src/phones/infrastructure/phones.entity";
 import { UserBirthDate } from "./value-objects/userBirthDate";
 import { userName } from "./value-objects/userName";
 import { UserGender } from "./value-objects/userGender";
 import { userId } from "./value-objects/userId";
+import { userSuscriptionState } from "./value-objects/userSuscriptionState";
+
 export class User {
   id: userId;
   name: userName;
   birth_date: UserBirthDate;
   genero: UserGender;
+  suscriptionState: userSuscriptionState;
   phonesNumber: PhonesNumber ;
   phone: PhoneEntity;
 
@@ -18,6 +20,7 @@ export class User {
     name: userName,
     birth_date: UserBirthDate,
     genero: UserGender,
+    suscriptionState: userSuscriptionState,
     phonesNumber:  PhonesNumber,
     phone: PhoneEntity,
   ) {
@@ -25,6 +28,7 @@ export class User {
     this.name = name;
     this.birth_date = birth_date;
     this.genero = genero;
+    this.suscriptionState = suscriptionState;
     this.phonesNumber = phonesNumber;
     this.phone = phone;
   }
