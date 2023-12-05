024f71dcf14f0485cce193137492045d5057c784
diegoargottez@gmail.com <diegoargottez@gmail.com>
2023-12-04 23:49:03 -0400

    [imp]:artist functional (not sure)

--- src/artists/infrastructure/controllers/artist.controller.ts
+++ src/artists/infrastructure/controllers/artist.controller.ts
@@ -5,11 +5,53 @@
 // import { GetSongByArtistId } from 'src/artists/application/services/GetSongsByArtistId.service';
 // import { Artist } from 'src/artists/domain/artist';
 
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Get } from "@nestjs/common";
 import { OrmArtistRepository } from "../repositories/artist.repository.impl";
+import { InjectRepository } from "@nestjs/typeorm";
+import { RegisterArtistApplicationServiceDto } from "src/artists/application/services/register-artist.application.service";
+import { CreateUserDto } from "src/users/application/dtos/create-user.dto";
+import { Result } from "src/common/domain/logic/Result";
+import { Post } from '@nestjs/common';
+import { Query } from "mysql2/typings/mysql/lib/protocol/sequences/Query";
+import { ArtistEntity } from "../entities/artist.entity";
+import { GetArtistProfilesApplicationService, GetArtistProfilesApplicationServiceDto } from "src/artists/application/services/get-artist-profile.application.service";
+import { GetArtistId } from "../get-artist-id.decorator";
+import { ResultMapper } from "src/common/Application/result-handler/result.mapper";
+import { Artist } from "src/artists/domain/artist";
+import { ArtistsMapper } from "../mappers/artist.mapper";
+import { ErrorApplicationServiceDecorator } from "src/common/Application/application-service/decorators/error-decorator/error-application.service.decorator";
+import { LoggingApplicationServiceDecorator } from "src/common/Application/application-service/decorators/error-decorator/loggin-application.service.decorator";
+import { NestLogger } from "src/common/infrastructure/logger/nest-logger";
 @Controller('artists')
 export class ArtistController {
-    private readonly ormArtistRepository: OrmArtistRepository;
+      private readonly ormArtistMapper: ArtistsMapper;
+constructor(
+    @InjectRepository(OrmArtistRepository)
+    private readonly ormArtistRepository: OrmArtistRepository
+) {}
+
+@Get('GetArtist')
+async getArtist(@GetArtistId() id):Promise<Result<ArtistEntity>>{ 
+    const dto: GetArtistProfilesApplicationServiceDto = {id};
+   // const service=new GetArtistProfilesApplicationServiceDto(this.ormArtistRepository);
+        //Mapeamos y retornamos.
+       
+        //Ejecutamos el caso de uso
+                //Creamos el servicio de aplicación.
+        const service = new ErrorApplicationServiceDecorator(
+                new LoggingApplicationServiceDecorator(
+                new GetArtistProfilesApplicationService(this.ormArtistRepository),
+                new NestLogger()
+            )
+        );
+        const result = (await service.execute(dto));
+        return ResultMapper.map(
+            result,
+            (value: Artist) => {
+                return this.ormArtistMapper.domainTo(value)
+            }
+        );
+}
 //   private findOneArtistService: FindOneArtistService;
 //   private findAllArtistService: FindAllArtistService;
 //   private findSongsByArtistIdService: GetSongByArtistId;
@@ -30,4 +72,4 @@ export class ArtistController {
 //   findSongsById(@Param('id') id: string): Promise<any> {
 //     return this.findSongsByArtistIdService.execute(id);
 //   }
-// }
+}
