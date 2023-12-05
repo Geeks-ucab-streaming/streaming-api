2afa593bd374d8b9681aa41c332961161b87a0b9
diegoargottez@gmail.com <diegoargottez@gmail.com>
2023-12-04 20:50:29 -0400

    [imp]: implement methods

--- src/artists/infrastructure/repositories/artist.repository.impl.ts
+++ src/artists/infrastructure/repositories/artist.repository.impl.ts
@@ -3,24 +3,33 @@ import { Artist } from 'src/artists/domain/artist';
 import { ArtistEntity } from '../entities/artist.entity';
 import { IArtistRepository } from 'src/artists/application/repositories/artist.repository.interface';
 import { ArtistID } from 'src/artists/domain/value-objects/artistID-valueobject';
+import { ArtistsMapper } from '../mappers/artist.mapper';
 
-export class OrmArtistRepository extends Repository<ArtistEntity> implements IArtistRepository {
-  // private readonly ormArtistMapper: OrmArtistMapper;
+export class OrmArtistRepository
+  extends Repository<ArtistEntity>
+  implements IArtistRepository
+{
+  private readonly ormArtistMapper: ArtistsMapper;
   constructor(manager: EntityManager, queryRunner?: QueryRunner) {
     super(ArtistEntity, manager, queryRunner);
     // this.ormPatientMapper = new OrmPatientMapper();
   }
-  findOneByTheId(id: ArtistID): Promise<Artist> {
-    throw new Error('Method not implemented.');
+  async findOneByTheId(id: ArtistID): Promise<Artist> {
+    const ormArtist = await this.findOne({ where: { id: id.Id } });
+    return ormArtist ? await this.ormArtistMapper.ToDomain(ormArtist) : null;
   }
   async saveAggregate(aggregate: Artist): Promise<void> {
-    // const ormArtist=await this.ormArtistMapper.fromDomainToOther(aggregate);
-    // await this.save(ormArtist);
-    throw new Error('Method not implemented.');
+    const ormArtist = await this.ormArtistMapper.domainTo(aggregate);
+     await this.save(ormArtist);
+    //throw new Error('Method not implemented.');
   }
   async findOneByIdOrFail(id: ArtistID): Promise<Artist> {
-    throw new Error('Method not implemented.');
-  }
+        const artist = await this.findOneByTheId(id);
+        if (!artist) {
+         // throw new InvalidPatientException();
+         throw new Error('Method not implemented.');
+        }
+        return artist;  }
 
   async findAssociatedArtists(): Promise<Artist[]> {
     throw new Error('Method not implemented.');
