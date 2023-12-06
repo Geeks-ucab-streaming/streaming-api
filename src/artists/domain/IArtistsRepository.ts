128693fab98f2598919989406e694fe4af49e7ab
diegoargottez@gmail.com <diegoargottez@gmail.com>
2023-12-06 02:19:24 -0400

    fixing artistsrepo

--- src/artists/domain/IArtistsRepository.ts
+++ src/artists/domain/IArtistsRepository.ts
@@ -1,6 +1,7 @@
 import { Artist } from './artist';
+import { ArtistID } from './value-objects/artistID-valueobject';
 
 export interface IArtistsRepository {
   findAllArtists(): Promise<Artist[]>;
-  findArtistById(id: string): Promise<Artist>;
+  findArtistById(id: ArtistID): Promise<Artist>;
 }
--- src/artists/infrastructure/repositories/artist.repository.impl.ts
+++ src/artists/infrastructure/repositories/artist.repository.impl.ts
@@ -1,23 +1,26 @@
 import { DataSource, EntityManager, QueryRunner, Repository } from 'typeorm';
 import { Artist } from 'src/artists/domain/artist';
 import { ArtistEntity } from '../entities/artist.entity';
-import { IArtistRepository } from 'src/artists/application/repositories/artist.repository.interface';
+
 import { ArtistID } from 'src/artists/domain/value-objects/artistID-valueobject';
 import { ArtistsMapper } from '../mappers/artist.mapper';
+import { IArtistsRepository } from 'src/artists/domain/IArtistsRepository';
 
 export class OrmArtistRepository
   extends Repository<ArtistEntity>
-  implements IArtistRepository
+  implements IArtistsRepository
 {
   private readonly ormArtistMapper: ArtistsMapper;
-  // constructor(manager: EntityManager, queryRunner?: QueryRunner) {
-  //   super(ArtistEntity, manager, queryRunner);
-  //   // this.ormPatientMapper = new OrmPatientMapper();
-  // }
-
   constructor(dataSource: DataSource) {
     super(ArtistEntity, dataSource.manager);
     this.ormArtistMapper = new ArtistsMapper();
+  }
+  findAllArtists(): Promise<Artist[]> {
+    throw new Error('Method not implemented.');
+  }
+  async findArtistById(id: ArtistID): Promise<Artist> {
+    const ormArtist = await this.findOne({ where: { id: id.Id } });
+    return ormArtist ? await this.ormArtistMapper.ToDomain(ormArtist) : null;
   }
   async findOneByTheId(id: ArtistID): Promise<Artist> {
     const ormArtist = await this.findOne({ where: { id: id.Id } });
