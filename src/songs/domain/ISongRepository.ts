e22fed64fa517b76169a6218e270f2885db3468a
diegoargottez@gmail.com <diegoargottez@gmail.com>
2023-12-16 09:31:04 -0400

    [IMP]: LogginDecorator to songs (get id

--- src/songs/domain/ISongRepository.ts
+++ src/songs/domain/ISongRepository.ts
@@ -1,7 +1,8 @@
 import { Song } from './song';
+import { SongID } from './value-objects/SongID-valueobject';
 
 export interface ISongRepository {
-  findById(id: string): Promise<Song>;
+  findById(id: SongID): Promise<Song>;
   findByArtistId(id: string): Promise<Song[]>;
   findByPlaylistId(id: string): Promise<Song[]>;
   findSongsInCollection(ids: string[]): Promise<Song[]>;
