83f17e19de13569028f8d2c37bcee086b3c6f2ed
Pico <mapico.20@est.ucab.edu.ve>
2023-12-23 11:49:35 -0400

    as

--- src/common/Application/application-service/decorators/error-decorator/audith-application.servive.decorator.ts
+++ src/common/Application/application-service/decorators/error-decorator/audith-application.servive.decorator.ts
@@ -5,24 +5,27 @@ import { ApplicationServiceDecorator } from "./application.service.decorator";
 import { Iaudith } from "src/common/Application/audith-handler/Iaudith";
 import { Audith } from "src/common/infrastructure/entities/audith.service.entity";
 import { audith_repo } from "src/common/repositories/repository-audith";
+import {v4 as uuidv4} from 'uuid';
 
 /**
  * Decorator class that adds auditing functionality to an application service.
  * @template JSON - The type of the input DTO.
  * @template R - The type of the result.
  */
-export class AudithServiceDecorator<JSON,Audith> extends ApplicationServiceDecorator<JSON,Audith> {
+export class AudithServiceDecorator<JSON,R> extends ApplicationServiceDecorator<JSON,R>{
 
-private audithrepo: audith_repo;
-private id: string;
+private audith:Audith ;
 
 
-  constructor(applicationservice: ApplicationServiceDecorator<JSON,Audith>,audith: Iaudith){
+  constructor(applicationservice: ApplicationServiceDecorator<JSON,R>,audith: audith_repo){
     super(applicationservice);
+    this.audith.id = uuidv4();
+    this.audith.origin = JSON.parse(JSON.stringify(applicationservice));
   }
 
-  async execute(dto: JSON): Promise<Result<Audith>> {
-    return Result.success<Audith>(await this.audithrepo.audith(this.id,dto));
+  async execute(dto: JSON): Promise<Result<R>> {
+
+    return await Result.success<Audith>(await(this.audith));
   }
 
   
