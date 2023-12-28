b820ac63f4c8be74079dad8714ade234e507da6d
LuisGil11 <Giluis2000@gmail.com>
2023-12-28 15:42:11 -0400

    singleton in calculateplaylistduration service implemented

--- src/playlist/domain/services/calculatePlaylistDuration.service.ts
+++ src/playlist/domain/services/calculatePlaylistDuration.service.ts
@@ -2,7 +2,18 @@ import { ISongRepository } from 'src/songs/domain/ISongRepository';
 import { Playlist } from '../playlist';
 import { Song } from 'src/songs/domain/song';
 
-export class calculatePlaylistDurationService {
+export class CalculatePlaylistDurationService {
+  static instance: CalculatePlaylistDurationService;
+  private constructor() {}
+
+  public static getInstance(): CalculatePlaylistDurationService {
+    if (!CalculatePlaylistDurationService.instance) {
+      CalculatePlaylistDurationService.instance =
+        new CalculatePlaylistDurationService();
+    }
+    return CalculatePlaylistDurationService.instance;
+  }
+
   async execute(playlist: Playlist, songRepository: ISongRepository) {
     let duration = 0;
     const songs: Song[] = await songRepository.findSongsInCollection(
