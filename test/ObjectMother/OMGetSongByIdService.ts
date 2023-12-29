0e0d22e2080b353af36c017463dd42a1ddc3d0a9
Pico <mapico.20@est.ucab.edu.ve>
2023-12-29 13:22:45 -0400

    Start of testing getsongbyidservice

---
+++ test/ObjectMother/OMGetSongByIdService.ts
@@ -0,0 +1,13 @@
+import { GetSongByIdService } from "src/songs/application/services/getSongById.service";
+import { GetSongByIdServiceDto } from "src/songs/application/services/getSongById.service";
+
+
+export class OMGetSongByIdService {
+
+public static GetSongByIdService(): GetSongByIdService {
+
+
+
+
+     }
+}
---
+++ test/services/service.test.ts
@@ -0,0 +1,8 @@
+import { GetSongByIdService } from "src/songs/application/services/getSongById.service";
+
+
+describe('GetSongByIdService', () => {
+
+
+
+})
