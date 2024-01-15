aa5901516c2f1039a1d8de8367bf97df4993f9e7
diegoargottez@gmail.com <diegoargottez@gmail.com>
2024-01-14 20:32:08 -0400

    [fix]: change audith entity name

--- src/common/infrastructure/entities/audith.entity.ts
+++ src/common/infrastructure/entities/audith.entity.ts
@@ -1,7 +1,7 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('Audith')
-export class PlaylistSongEntity {
+export class AudithEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
