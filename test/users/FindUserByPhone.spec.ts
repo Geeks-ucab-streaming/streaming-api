91cfede13affd93fe3236edfdae47bac93d0239e
diegoargottez@gmail.com <diegoargottez@gmail.com>
2024-01-16 18:51:50 -0400

    [ref]: ts names

--- test/users/FindUserByPhone.spec.ts
+++ test/users/FindUserByPhone.spec.ts
@@ -1,6 +1,6 @@
 import { DataSourceSingleton } from '../../src/common/infrastructure/dataSourceSingleton';
 import { TransactionHandlerImplementation } from '../../src/common/infrastructure/transaction_handler_implementation';
-import { FindUserByPhoneTest } from '../../test/core/objects-mother/signUserInTest';
+import { FindUserByPhoneTest } from '../core/objects-mother/FindUserByPhoneTest';
 import { UserObjectMother } from '../../test/core/objects-mother/user.object-mother';
 import { UserRepositoryMock } from '../../test/core/repository-mocks/user.repository.mock';
 
