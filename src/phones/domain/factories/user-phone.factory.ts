f8769788b7fb1c74807e5cd596eca1e248d26e14
Andrés <95057542+andreselc@users.noreply.github.com>
2024-01-16 19:55:44 -0400

    phoneFactory was put in its aggregate

---
+++ src/phones/domain/factories/user-phone.factory.ts
@@ -0,0 +1,13 @@
+import { phoneId } from "src/phones/domain/phoneAggregate/value-objects/phoneId";
+import { phoneNumber } from "src/phones/domain/phoneAggregate/value-objects/phoneNumber";
+import { phoneOperatorsEnum } from "../phoneAggregate/value-objects/phoneOperators.enum";
+import { Phone } from "src/phones/domain/phoneAggregate/phone";
+import { Line } from "src/phones/domain/phoneAggregate/value-objects/line";
+import { PhoneParameterObject } from "src/phones/domain/parameterObjects/phoneParameterObject";
+
+export class UserPhoneFactory {
+  public static phoneFactoryMethod(phone: PhoneParameterObject): Phone {
+        return new Phone(phoneId.create(phone.idPhone), phoneNumber.create(phone.phoneNumber), Line.create(phone.idLine,Object.keys(phoneOperatorsEnum).find(key => key === phone.line.toUpperCase()
+         )));
+    }
+}
