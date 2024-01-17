2dbde797c2c79c30130a1495873b3ecf7ca4241a
gamboaalejandro <agamboacj@gmail.com>
2024-01-16 22:37:40 -0400

    fix imports

--- test/users/FindUserByPhone.spec.ts
+++ test/users/FindUserByPhone.spec.ts
@@ -1,5 +1,4 @@
-import { DataSourceSingleton } from '../../src/common/infrastructure/dataSourceSingleton';
-import { TransactionHandlerImplementation } from '../../src/common/infrastructure/transaction_handler_implementation';
+
 import { FindUserByPhoneTest } from '../core/objects-mother/FindUserByPhoneTest';
 import { UserObjectMother } from '../../test/core/objects-mother/user.object-mother';
 import { UserRepositoryMock } from '../../test/core/repository-mocks/user.repository.mock';
@@ -9,7 +8,6 @@ describe('FindUserByPhoneService!!', () => {
     //Arrange
     const user = UserObjectMother.createUser();
     const mock = new UserRepositoryMock();
-
     const service = FindUserByPhoneTest.findUserByPhoneService(mock, null);
 
     //Act
