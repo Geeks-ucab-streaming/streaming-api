f51d705c46d74ccc9fc3fc48ce0a1fc89cc47eec
diegoargottez@gmail.com <diegoargottez@gmail.com>
2024-01-17 11:27:16 -0400

    [imp]: audith deco on /top_playlists

--- src/playlist/infrastructure/controllers/playlist.controller.ts
+++ src/playlist/infrastructure/controllers/playlist.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Inject, Param, ParseUUIDPipe } from '@nestjs/common';
+import { Controller, Get, Inject, Param, ParseUUIDPipe, Req, UseGuards } from '@nestjs/common';
 import {
   FindAlbumByArtistIDService,
   FindAlbumByArtistIDServiceDto,
@@ -10,7 +10,7 @@ import {
 import { Playlist } from 'src/playlist/domain/playlist';
 import { PlaylistRepository } from '../PlaylistRepository.impl';
 import { DataSourceSingleton } from 'src/common/infrastructure/dataSourceSingleton';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { OrmSongRepository } from 'src/songs/infrastructure/repositories/song.repository.impl';
 import { FindTopPlaylistsService } from 'src/playlist/application/services/FindTopPlaylists.service';
 import { PlaylistDto, SongDto, TopPlaylistDto } from 'src/dtos';
@@ -27,7 +27,11 @@ import { MyResponse } from 'src/common/infrastructure/Response';
 import { SongID } from 'src/songs/domain/value-objects/SongID-valueobject';
 import { PlaylistID } from 'src/playlist/domain/value-objects/PlaylistID-valueobject';
 import { ArtistID } from 'src/artists/domain/value-objects/artistID-valueobject';
-
+import { AudithApplicationServiceDecorator } from 'src/common/Application/application-service/decorators/error-decorator/audith.service.decorator';
+import { AudithRepositoryImpl } from 'src/common/infrastructure/repositories/audithRepository.impl';
+import { JwtService } from '@nestjs/jwt';
+import { JwtAuthGuard } from 'src/users/application/jwtoken/jwt-auth.guard';
+@ApiBearerAuth()
 @Controller('api/playlist')
 export class PlaylistController {
   private repository: PlaylistRepository;
@@ -38,6 +42,8 @@ export class PlaylistController {
   private findTopPlaylistsService: FindTopPlaylistsService;
   private findSongsInCollectionService: GetSongsInCollectionService;
   private findArtistsInCollectionService: FindArtistsInCollectionService;
+  private audithRepo: AudithRepositoryImpl;
+  private jwtService: JwtService = new JwtService();
 
   constructor() {
     this.repository = new PlaylistRepository(DataSourceSingleton.getInstance());
@@ -47,20 +53,30 @@ export class PlaylistController {
     this.artistsRepository = new OrmArtistRepository(
       DataSourceSingleton.getInstance(),
     );
+    this.audithRepo = new AudithRepositoryImpl();
   }
-
+  
+  @UseGuards(JwtAuthGuard)
   @ApiTags('TopPlaylist')
   @Get('/top_playlists')
-  async findTopPlaylists(): Promise<MyResponse<TopPlaylistDto>> {
-    this.findTopPlaylistsService = new FindTopPlaylistsService(
-      this.repository,
-      this.songRepository,
+  async findTopPlaylists(
+    @Req() req: Request,
+  ): Promise<MyResponse<TopPlaylistDto>> {
+    const token = req.headers['authorization']?.split(' ')[1] ?? '';
+    const userid = await this.jwtService.decode(token).id;
+    const service = new AudithApplicationServiceDecorator(
+      new FindTopPlaylistsService(this.repository, this.songRepository),
+      this.audithRepo,
+      userid,
     );
+    // this.findTopPlaylistsService = new FindTopPlaylistsService(
+    //   this.repository,
+    //   this.songRepository,
+    // );
     let topPlaylistsInfo: TopPlaylistDto = {
       playlists: [],
     };
-    const playlistsResponse: Result<Playlist[]> =
-      await this.findTopPlaylistsService.execute();
+    const playlistsResponse: Result<Playlist[]> = await service.execute();
     if (playlistsResponse.IsSuccess) {
       const playlistsResult: Playlist[] = (
         await this.findTopPlaylistsService.execute()
