1df9825e75ad1d202d8dc920ae4cc01c4ff37f53
diegoargottez@gmail.com <diegoargottez@gmail.com>
2024-01-17 02:10:37 -0400

    [imp]:test signuserin (failing)

--- test/users/FindUserByPhone.spec.ts
+++ test/users/FindUserByPhone.spec.ts
@@ -2,7 +2,7 @@ import { FindUserByPhoneTest } from '../core/objects-mother/FindUserByPhoneTest'
 import { UserRepositoryMock } from '../../test/core/repository-mocks/user.repository.mock';
 import { UserObjectMother } from 'test/core/objects-mother/user.object-mother';
 
-describe('FindUserByPhoneService!!', () => {
+describe('Testing FindUserByPhoneService', () => {
   test('test', async () => {
     //Arrange
     const user = UserObjectMother.createUser();
